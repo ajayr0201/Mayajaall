7786d48e1fbd7b5d56a3abb9abfdfea5d470df9a
ajayr0201 <123584148+ajayr0201@users.noreply.github.com>
2026-09-24 21:00:02 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mayajaal',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mayajaal App'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to Mayajaal!',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
