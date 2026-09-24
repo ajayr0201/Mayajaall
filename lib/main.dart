fb7627c64826b277d6a687181b2dd230080ebf89
ajayr0201 <123584148+ajayr0201@users.noreply.github.com>
2026-09-24 23:01:50 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-import 'package:uni_links/uni_links.dart';
-import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -32,49 +30,13 @@ class VideoPlayerScreen extends StatefulWidget {
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   VideoPlayerController? _controller;
-  String currentVideoUrl = 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
+  final String currentVideoUrl = 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
   bool isInitialized = false;
-  StreamSubscription? _sub;
 
   @override
   void initState() {
     super.initState();
-    _initIncomingLinks();
     _initPlayer(currentVideoUrl);
-  }
-
-  // Incoming deep links ko pakadne ke liye
-  void _initIncomingLinks() async {
-    try {
-      // Agar app link se khula hai
-      final initialUri = await getInitialUri();
-      if (initialUri != null) {
-        _handleUri(initialUri);
-      }
-
-      // Agar app already background mein hai aur link click hua
-      _sub = uriLinkStream.listen((Uri? uri) {
-        if (uri != null) {
-          _handleUri(uri);
-        }
-      }, onError: (err) {
-        debugPrint('Failed to receive uri: $err');
-      });
-    } catch (e) {
-      debugPrint('Error handling links: $e');
-    }
-  }
-
-  void _handleUri(Uri uri) {
-    // URL se video parameter nikalna (jaise: mayajaal.com/?video=DIRECT_URL)
-    final videoParam = uri.queryParameters['video'];
-    if (videoParam != null && videoParam.isNotEmpty) {
-      setState(() {
-        currentVideoUrl = videoParam;
-        isInitialized = false;
-      });
-      _initPlayer(currentVideoUrl);
-    }
   }
 
   void _initPlayer(String url) {
@@ -90,7 +52,6 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   @override
   void dispose() {
-    _sub?.cancel();
     _controller?.dispose();
     super.dispose();
   }
