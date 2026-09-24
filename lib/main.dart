7b49e68a7730aa6cb23685e65b048d57e9ff33bc
ajayr0201 <123584148+ajayr0201@users.noreply.github.com>
2026-09-24 22:27:57 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,16 +30,22 @@ class VideoApp extends StatefulWidget {
 }
 
 class _VideoAppState extends State<VideoApp> {
-  late VideoPlayerController _controller;
+  VideoPlayerController? _controller;
+  // Default sample video, jo baad mein link se replace ho jayegi
+  String videoUrl = 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(
-      Uri.parse('https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'),
-    )..initialize().then((_) {
+    _initPlayer(videoUrl);
+  }
+
+  void _initPlayer(String url) {
+    _controller?.dispose();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(url))
+      ..initialize().then((_) {
         setState(() {});
-        _controller.play();
+        _controller?.play();
       });
   }
 
@@ -49,23 +56,25 @@ class _VideoAppState extends State<VideoApp> {
         title: const Text('Mayajaal Video Player'),
       ),
       body: Center(
-        child: _controller.value.isInitialized
+        child: _controller != null && _controller!.value.isInitialized
             ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
+                aspectRatio: _controller!.value.aspectRatio,
+                child: VideoPlayer(_controller!),
               )
             : const CircularProgressIndicator(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            _controller.value.isPlaying
-                ? _controller.pause()
-                : _controller.play();
+            _controller!.value.isPlaying
+                ? _controller!.pause()
+                : _controller!.play();
           });
         },
         child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          _controller != null && _controller!.value.isPlaying
+              ? Icons.pause
+              : Icons.play_arrow,
         ),
       ),
     );
@@ -73,7 +82,7 @@ class _VideoAppState extends State<VideoApp> {
 
   @override
   void dispose() {
-    _controller.dispose();
+    _controller?.dispose();
     super.dispose();
   }
 }
