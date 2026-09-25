93b12547b1a101dd4a1db97d16c68cb82141b00d
ajayr0201 <123584148+ajayr0201@users.noreply.github.com>
2026-09-25 19:10:30 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,11 @@ import 'package:webview_flutter/webview_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-// ⚠️ YAHAN APNI SUPABASE DETAILS DAALO
-const String supabaseUrl = 'YOUR_SUPABASE_URL';
-const String supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+// ⚠️ SUPABASE DETAILS
+const String supabaseUrl = 'https://qajayr0201-9102.supabase.co';
+const String supabaseAnonKey = 'sb_publishable_6b9xe3mDBduO-soZTk3t2A_W1sQpD5K';
+
+// ⚠️ GOOGLE WEB CLIENT ID
 const String webClientId = '985001671962-rok8qnng0rumjsd8mgr8uhr92o5vhs4n.apps.googleusercontent.com';
 
 Future<void> main() async {
